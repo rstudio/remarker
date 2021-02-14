6d6b2daaa2d733d2cf4d5716b4896e53b602c3a4
Winston Chang <winston@stdout.org>
2021-02-14 12:53:49 -0600

    Clearer function names

---
+++ R/md_ast.R
@@ -0,0 +1,41 @@
+pandoc_markdown_format <- paste0(
+  "markdown",
+  "+backtick_code_blocks",
+  "+fenced_code_attributes",
+  "+yaml_metadata_block",
+  "-hard_line_breaks"
+)
+
+#' @export
+md_ast <- function(file) {
+  js <- system2("pandoc",
+    args = c(
+      "-f", pandoc_markdown_format,
+      "-t", "json",
+      "-s", file
+    ),
+    stdout = TRUE
+  )
+
+  json <- jsonlite::fromJSON(js, simplifyDataFrame = FALSE, simplifyVector = FALSE)
+  add_class(json,  "pandoc_ast")
+}
+
+#' @export
+ast_md <- function(ast, outfile = NULL) {
+  stopifnot(inherits(x, "pandoc_ast"))
+
+  tmpfile_json <- tempfile()
+  json <- toJSON(ast, auto_unbox = TRUE)
+  writeLines(json, tmpfile_json)
+  system2(
+    "pandoc",
+    args = c(
+      "-f", "json",
+      "-t", pandoc_markdown_format,
+      "--wrap=preserve",
+      tmpfile_json,
+      if (!is.null(outfile)) c("-o", outfile)
+    )
+  )
+}
