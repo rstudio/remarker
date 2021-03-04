d11b45db382a01f43e7d786a68e7a83232f863bb
Winston Chang <winston@stdout.org>
2021-03-03 19:40:09 -0600

    Print indices

--- R/print.R
+++ R/print.R
@@ -1,5 +1,5 @@
 
-indent_string <- function(branches) {
+indent_string <- function(branches, branch_label = NULL) {
   if (length(branches) == 0) {
     return("")
   }
@@ -7,15 +7,26 @@ indent_string <- function(branches) {
   res <- character(length(branches))
 
   # Vertical bar (like "|" but taller)
-  res[branches == "TRUE" ]       <- "\U2502  "
-  res[branches == "FALSE"]       <- "   "
+  res[branches == "TRUE" ]       <- "\u2502   "
+  res[branches == "FALSE"]       <- "    "
+
+  if (is.null(branch_label)) {
+    branch_label <- "\u2500\u2500"
+
+  } else {
+    branch_label <- as.character(branch_label)
+
+    if (nchar(branch_label) == 1) {
+      branch_label <- paste0("\u2500", branch_label)
+    }
+  }
 
   if (branches[length(branches)] == "TRUE") {
     # Vertical bar with line right (sort of like "|-")
-    res[length(branches)] <- "\U251c──"
+    res[length(branches)] <- paste0("\u251c", branch_label, "\u2500")
   }
   # Lines up, right (sort of like a bottom left corner)
-  res[branches == "LAST_CHILD" ] <- "\U2514──"
+  res[branches == "LAST_CHILD" ] <- paste0("\u2514", branch_label, "\u2500")
   crayon::silver(paste(res, collapse = ""))
 }
 
@@ -24,9 +35,19 @@ cat0 <- function(...) {
 }
 
 #' @export
-print.Element <- function(x, ..., branches = character(0)) {
+print.Element <- function(x, ..., branches = character(0), branch_label = NULL) {
   classname <- class(x)[1]
-  cat0("\n", indent_string(branches), crayon::silver("<", classname, ">", sep = ""))
+
+  if (is.null(names(x))) {
+    delims <- c("[", "]")
+  } else {
+    delims <- c("{", "}")
+  }
+
+  cat0(
+    "\n", indent_string(branches, branch_label),
+    crayon::silver(delims[1], classname, delims[2], sep = "")
+  )
 
   # If this is the last child, then for any children, the shouldn't be a branch
   # at this level.
@@ -44,17 +65,15 @@ print.Element <- function(x, ..., branches = character(0)) {
       # Do nothing
 
     } else if (length(content) == 1) {
-      cat("c:")
       if (is.atomic(content)) {
         # Special case for strings and numbers: keep on same line
-        cat0(crayon::green('"', escape_str(content), '"', sep = ""))
+        cat0("c:", crayon::green('"', escape_str(content), '"', sep = ""))
       } else {
-        print(content, branches = c(branches, "LAST_CHILD"))
+        print(content, branches = c(branches, "LAST_CHILD"), branch_label = "c")
       }
 
     } else {
-      cat("c:")
-      print(content, branches = c(branches, "LAST_CHILD"))
+      print(content, branches = c(branches, "LAST_CHILD"), branch_label = "c")
     }
 
   } else if (is_unnamed(x)) {
@@ -63,7 +82,7 @@ print.Element <- function(x, ..., branches = character(0)) {
     branches[length(branches) + 1] <- "TRUE"
     for (i in seq_along(x)) {
       if (i == length(x)) branches[length(branches)] <- "LAST_CHILD"
-      print(x[[i]], branches = branches)
+      print(x[[i]], branches = branches, branch_label = i)
     }
 
   } else {
@@ -75,15 +94,15 @@ print.Element <- function(x, ..., branches = character(0)) {
 # Override some base S3 methods in the context of this package.
 # =====================================================================
 
-print.character <- function(x, ..., branches = character(0)) {
+print.character <- function(x, ..., branches = character(0), branch_label = NULL) {
   cat0(
-    "\n", indent_string(branches),
+    "\n", indent_string(branches, branch_label),
      crayon::green('"', escape_str(x), '"', sep = "")
   )
 }
 
-print.integer <- function(x, ..., branches = character(0)) {
-  cat0("\n", indent_string(branches), crayon::green(x))
+print.integer <- function(x, ..., branches = character(0), branch_label = NULL) {
+  cat0("\n", indent_string(branches, branch_label), crayon::green(x))
 }
 
 print.numeric <- print.character
@@ -92,5 +111,5 @@ print.list <- print.Element
 
 
 escape_str <- function(x) {
-  sub('"', '\\"', x, fixed = TRUE)
+  gsub('"', '\\"', x, fixed = TRUE)
 }
