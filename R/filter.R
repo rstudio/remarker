bb962d894444e005bf6e6e2060657fa8bde42b1c
Winston Chang <winston@stdout.org>
2021-03-05 17:02:36 -0600

    Support Pandoc and Meta filters

--- R/filter.R
+++ R/filter.R
@@ -5,7 +5,21 @@ ast_filter <- function(x, filters) {
   for (i in seq_along(filters)) {
     filter_set <- filters[[i]]
     category <- names(filters)[i]
-    x <- ast_walk(x, filter_set, category)
+
+    if (category == "Pandoc") {
+      # Pandoc and Meta are special-cased; Pandoc only operates on the top-level
+      # object, and Meta only operates on the $meta field, so no need to
+      # walk the tree.
+      if (length(filter_set) != 0) {
+        x <- apply_filter(x, filter_set$Pandoc)
+      }
+    } else if (category == "Meta") {
+      if (length(filter_set) != 0) {
+        x$meta <- apply_filter(x$meta, filter_set$Meta)
+      }
+    } else {
+      x <- ast_walk(x, filter_set, category)
+    }
   }
 
   x
@@ -44,46 +58,57 @@ ast_walk <- function(x, filters, category) {
     }
 
     # If we get here, we have a filter function for this element.
-    res <- filter(x)
-    new_category <- class(res)[1]
-
-    if (is.null(res)) {
-      # Filter returned NULL; leave object unchanged.
-
-    } else if (new_category == category) {
-      # Filter returned a Block or Inline object, same as input.
-      x <- res
-
-    } else if (new_category == paste0(category, "s")) {
-      # Filter returned a list of input objects, which we need to splice.
-      stop("Splicing lists not implemented yet.")
-
-    } else {
-      stop('Category of returned object does not match. Original: "',
-        category, '"  New: "', new_category, '"'
-      )
-    }
+    x <- apply_filter(x, filter)
   }
 
   # Recurse. We don't need to recurse into every possible kind of child list --
   # only the ones listed here.
-  # Maybe this should be a for-loop to support splicing?
   if (x_cat == "Inline" || x_cat == "Block") {
     if (is.list(x[["c"]])) {
       x[["c"]][] <- lapply(x[["c"]], ast_walk, filters, category)
     }
   } else if (x_cat %in% c("Inlines", "Blocks", "Inliness", "Blockss", "Pandoc", "Meta")) {
     if (is.list(x)) {
-      x[] <- lapply(x, ast_walk, filters, category)
+      for (i in seq_along(x)) {
+        x[[i]] <- ast_walk(x[[i]], filters, category)
+      }
+      # x[] <- lapply(x, ast_walk, filters, category)
     }
   }
 
   x
 }
 
+# Call filter(x), and do some checking on the result.
+# If filter(x) returns NULL, return the original object.
+# If it returns an object, make sure the category matches.
+apply_filter <- function(x, filter) {
+  old_category <- class(x)[1]
+
+  res <- filter(x)
+  new_category <- class(res)[1]
+
+  if (is.null(res)) {
+    # Filter returned NULL; leave object unchanged.
+    return(x)
+
+  } else if (new_category == old_category) {
+    # Filter returned a Block or Inline object, same as input.
+    return(res)
+
+  } else if (new_category == paste0(old_category, "s")) {
+    # Filter returned a list of input objects, which we need to splice.
+    stop("Splicing lists not implemented yet.")
+  }
+
+  stop('Category of returned object does not match. Original: "',
+    old_category, '"  New: "', new_category, '"'
+  )
+}
+
 
 # Given a flat list of filter functions like:
-#   list(Inline = f, Para = f, Str = f, Inlines = f)
+#   list(Inline = f, Para = f, Str = f, Pandoc = f, Inlines = f)
 # return a list with structure like:
 #   list(
 #     Inline  = list(Inline = f, Str = f),
@@ -91,7 +116,7 @@ ast_walk <- function(x, filters, category) {
 #     Block   = list(Para = f)
 #     Blocks  = list()
 #     Meta    = list(),
-#     Pandoc  = list()
+#     Pandoc  = list(Pandoc = f)
 #   )
 #
 # Note that within the Inline category, the functions are not sorted. The
@@ -109,7 +134,14 @@ categorize_filters <- function(filters) {
   filter_categories <- c("Inline", "Inlines", "Block", "Blocks", "Meta", "Pandoc")
 
   category_map <- vapply(ast_types, `[[`, "category", FUN.VALUE = "")
-  category_map <- c(category_map, Inline = "Inline", Block = "Block")
+  # Add a few types that aren't listed in ast_types
+  category_map <- c(
+    category_map,
+    Inline = "Inline",
+    Block  = "Block",
+    Meta   = "Meta",
+    Pandoc = "Pandoc"
+  )
 
   filter_names <- names(filters)
 
