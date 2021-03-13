70a33cd6f6b736c7daeb9d8067b07f3c53ede5eb
Winston Chang <winston@stdout.org>
2021-03-13 00:02:52 -0600

    Add nest_sections

---
+++ R/nest.R
@@ -0,0 +1,93 @@
+#' @export
+nest_sections <- function(x) {
+  if (inherits(x, "Pandoc")) {
+    x$blocks <- nest_sections(x$blocks)
+    return(x)
+  }
+
+  # TODO: Add type check -- maybe need to move implementation out to separate
+  # function?
+
+  # Find the depth of a Header; if not a Header, return 99
+  depth <- function(obj) {
+    if (obj$t == "Header") obj$c[[1]]
+    else                   99L
+  }
+
+  res <- Blocks()
+
+  # In x, the starting index of the current section
+  section_start_idx <- 1L
+  min_depth <- 99L
+
+  for (i in seq_along(x)) {
+    cur_depth <- depth(x[[i]])
+
+    # Within a section, any higher order blocks
+    if (min_depth == 99L) {
+      if (cur_depth == min_depth) {
+        res[[length(res) + 1L]] <- x[[i]]
+
+      } else if (cur_depth < min_depth) {
+        section_start_idx <- i
+      }
+
+    } else if (cur_depth <= min_depth || i == length(x)) {
+      # Enter into here when we hit a header that has lower number (which is a
+      # "higher-level" header), or when we hit the last block.
+
+      # The previous section has ended.
+      res[[length(res) + 1]] <- NestedSection(
+        # Header block
+        x[[section_start_idx]],
+        # All the remaining blocks until the next section
+        nest_sections(x[seq(section_start_idx+1, i-1)])
+      )
+
+      # This block is the start of a new section
+      section_start_idx <- i
+    }
+
+    if (cur_depth < min_depth) {
+      min_depth <- cur_depth
+    }
+  }
+
+  res
+}
+
+
+#' @export
+unnest_sections <- function(x) {
+  if (inherits(x, "Pandoc")) {
+    x$blocks <- unnest_sections(x$blocks)
+    return(x)
+  }
+
+  if (inherits(x, "Blocks")) {
+    res <- lapply(x, unnest_sections)
+    res <- unlist(res, recursive = FALSE)
+    res <- do.call(Blocks, res)
+    return(res)
+  }
+
+  if (!inherits(x, "Block")) {
+    stop("x must be a Blocks or Block object")
+  }
+
+  if (x$t != "NestedSection") {
+    return(list(x))
+  }
+  # If we get here, it must be a NestedSection object.
+
+  res <- Blocks()
+
+  # Pull the Header block up
+  res[[1]] <- x$c[[1]]
+
+  # Unnest all the children
+  children <- unnest_sections(x$c[[2]])
+  res[1 + seq_along(children)] <- children
+
+  res
+}
